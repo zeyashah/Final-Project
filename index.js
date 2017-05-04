a2458189d3b668b936628512b5ed087b4201d70d
Ping Louie <PingLouie11@gmail.com>
2017-05-04 17:24:43 -0400

    added 2nd carousel

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ $(document).ready(function(){
 	$(".mainCarousel").slick({
 	  dots: true,
 	  infinite: true,
+	  speed: 800,
+	  fade: true,
+	  cssEase: 'linear',
+	  autoplay: true,
+	});
+});
+
+$(document).ready(function(){
+	$(".smallCarousel").slick({
+	  dots: true,
+	  infinite: true,
 	  speed: 500,
 	  fade: true,
-	  cssEase: 'linear'
+	  cssEase: 'linear',
+	  autoplay: true,
 	});
 });
