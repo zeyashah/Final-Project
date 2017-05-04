aad1ac17381017e50547fbd60c8766e30dba27a7
Ping Louie <PingLouie11@gmail.com>
2017-05-04 13:41:59 -0400

    working on carousel

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-/*$(document).ready(function(){
-  $(".mainCarousel").slick({
-    setting-name: setting-value
-  });
-});*/
-
-$(".mainCarousel").slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
+$(document).ready(function(){
+	$(".mainCarousel").slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear'
+	});
 });
