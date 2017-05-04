4e72433bd0c1052d79d2aa68b1582f3a52da12f0
Ping Louie <PingLouie11@gmail.com>
2017-05-03 20:24:33 -0400

    working on nav

---
+++ index.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function(){
+  $(".mainCarousel").slick({
+    setting-name: setting-value
+  });
+});*/
+
+$(".mainCarousel").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
