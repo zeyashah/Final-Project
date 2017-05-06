da9b782e026a14f41f64da38d79b3e434f603456
Ping Louie <PingLouie11@gmail.com>
2017-05-06 13:23:22 -0400

    search box works

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ $(document).ready(function(){
 	  autoplay: true,
 	});
 });
+
+$("#search").keypress(function(event) {
+	if (event.keyCode === 13){
+		var word = $("#search").val();
+		$("body").unmark()
+		$('body').mark(word)
+		console.log($("#search").val())
+	}
+})
